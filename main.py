d2b8523e1947317df058abb80dd98f0d07db9098
Timur Turatbekov <timatur242005@gmail.com>
2021-08-29 03:41:59 +0300

    business and factory previous balance

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ from math import floor
 
 ''' 
 # Business constants
-max_level = 125
 start_deposit = 2
 degree_of_level = 2
 degree_of_dependence = 2
 start_time = 1
 end_time = 20160
+max_level = 125
 '''
 
 # Factory constants
-max_level = 125
+# Production speed
+degree_of_level = 1.5
 start_deposit = 2
-degree_of_level = 2
+# Time
 degree_of_dependence = 2
-start_time = 1
-end_time = 20160
+start_time = 2
+end_time = 43200
+
+max_level = 80
 
 levels = list()
 upgrade_values = list()
@@ -31,19 +34,29 @@ time_days = list()
 
 for level in range(max_level):
     levels.append(level + 1)
+    # Linear interpolation between start and end time
     time_minutes.append(1 + (levels[level] / max_level) ** degree_of_dependence * (end_time - start_time))
+    # Production = level^a * b
     production.append(floor(levels[level] ** degree_of_level * start_deposit))
-    upgrade_values.append(floor(time_minutes[level] * levels[level] ** degree_of_level * start_deposit))
+    # S = Production * Time
+    upgrade_values.append(floor(time_minutes[level] * production[level]))
+    # Time representation
     time_hours.append(floor(time_minutes[level] / 60))
     time_days.append(floor(time_minutes[level] / 1440))
 
 fig = go.Figure(data=[go.Table(header=dict(values=['Level', 'Production', 'Value for upgrade',
                                                    'Time to next level (minutes)',
-                                                   'Time to next level (hours)', 'Time to next level (days)']),
-                               cells=dict(values=[levels, production, upgrade_values, time_minutes, time_hours, time_days]))
+                                                   'Time to next level (hours)',
+                                                   'Time to next level (days)'],
+                                           fill_color='paleturquoise',
+                                           align='right'),
+                               cells=dict(values=[levels, production, upgrade_values,
+                                                  time_minutes, time_hours, time_days],
+                                          fill_color='lavender',
+                                          align='right'))
                       ])
 
-fig.update_layout(width=1920, height=1080)
+fig.update_layout(width=1280, height=720)
 
 app = dash.Dash()
 app.layout = html.Div([
