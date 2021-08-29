5fe4237202bf14be17393d1616d382a4f8210d31
Timur Turatbekov <timatur242005@gmail.com>
2021-08-29 09:54:55 +0300

    Start values

--- main.py
+++ main.py
@@ -18,19 +18,20 @@ with st.form(key='constants_form'):
     left_column, right_column = st.columns(2)
 
     left_column.write('Множители')
-    a = left_column.number_input(label='a (Амплитуда главной кривой)', value=1.0, step=0.1)
-    b = left_column.number_input(label='b (Амплитуда волн)', value=1.0, step=0.1)
-    c = left_column.number_input(label='c', value=1.0, step=0.1)
+    a = left_column.number_input(label='a (Амплитуда главной кривой)', value=2.0, step=0.1)
+    b = left_column.number_input(label='b (Амплитуда волн)', value=40.0, step=0.1)
+    c = left_column.number_input(label='c', value=0.2, step=0.1)
     
     right_column.write('Степени')
     k = right_column.number_input(label='k (Склон главной кривой)', value=3.0, step=0.1)
-    m = right_column.number_input(label='m (Растяжение амплитуды волн со временем)', value=0.5, step=0.1)
-    n = right_column.number_input(label='n (Растяжение периода волн со временем)', value=0.5, step=0.1)
+    m = right_column.number_input(label='m (Растяжение амплитуды волн со временем)', value=1.5, step=0.1)
+    n = right_column.number_input(label='n (Растяжение периода волн со временем)', value=1.5, step=0.1)
     
     '''
     ## Дополнительные параметры
     '''
     max_level = st.number_input(label='Максимальный уровень', value=1, min_value=1)
+    smoothed = st.checkbox('Сглаженная кривая')
 
     submit_button = st.form_submit_button(label='Смоделировать')
 
@@ -40,7 +41,6 @@ with st.form(key='constants_form'):
 
 st.latex(fr'''y = {a}x^{{{k}}} + {b}x^{{{m}}} * |\sin({c}x^{{{n}}})|''')
 
-
 def volumeToUp(x):
     return a * np.power(x, k) + b * np.power(x, m) * np.absolute(np.sin(c * np.power(x, n)))
 
@@ -49,19 +49,29 @@ if submit_button:
     levels = np.arange(max_level) + 1
     volume_to_up = volumeToUp(levels)
 
-    levels_smoothed = np.arange(max_level, step=0.05) + 1
-    volume_to_up_smoothed = volumeToUp(levels_smoothed)
-    volume_to_up_main_curve_smooothed = a * np.power(levels_smoothed, k);
-
     df = pd.DataFrame({
         'Уровень': levels,
         'Объем для повышения уровня': volume_to_up,
-    })
+    }, index=levels)
 
-    chart_df = pd.DataFrame({
-        'Объем для повышения уровня': volume_to_up_smoothed,
-        'Объем для повышения уровня (без волн)': volume_to_up_main_curve_smooothed,
-    })
+    if smoothed:
+        levels_smoothed = np.arange(1, max_level + 0.05, step=0.05)
+        volume_to_up_smoothed = volumeToUp(levels_smoothed)
+        volume_to_up_main_curve_smooothed = a * np.power(levels_smoothed, k)
+
+        chart_df = pd.DataFrame({
+            'Объем для повышения уровня': volume_to_up_smoothed,
+            'Без волн': volume_to_up_main_curve_smooothed,
+        }, index=levels_smoothed)
+    else:
+        volume_to_up_main_curve = a * np.power(levels, k)
+
+        chart_df = pd.DataFrame({
+            'Объем для повышения уровня': volume_to_up,
+            'Без волн': volume_to_up_main_curve,
+        }, index=levels)
+
+    
 
     st.dataframe(df.style.format('{:.0f}'))
 
